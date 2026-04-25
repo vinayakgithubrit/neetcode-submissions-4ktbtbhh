88528285e2bc13ae3a0bb210c2f4902a4881f04e
Vinayak Gumasta <vinayakgumasta10@gmail.com>
2026-04-26 01:08:00 +0530

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-0.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    void createmapping(vector<int> &inorder,map<int,int> &nodetoindex,int &n){
+        for(int i = 0; i < n; i++){
+            nodetoindex[inorder[i]] = i;
+        }
+    }
+    
+    TreeNode* solve(vector<int> &inorder, vector<int> &preorder,int &index,int inorderstart,int inorderend,int &n,map<int,int> &nodetoindex){
+        if(index > n || inorderstart > inorderend){
+            return NULL;
+        }
+        
+        int element = preorder[index++];
+        TreeNode* root = new TreeNode(element);
+        int pos = nodetoindex[element];
+        
+        root->left = solve(inorder,preorder,index,inorderstart,pos-1,n,nodetoindex);
+        root->right = solve(inorder,preorder,index,pos+1,inorderend,n,nodetoindex);
+        
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = inorder.size();
+        int preindex = 0;
+        map<int,int> nodetoindex;
+        createmapping(inorder,nodetoindex,n);
+        TreeNode* ans = solve(inorder,preorder,preindex,0,n-1,n,nodetoindex);
+        return ans;
+    }
+};
